0a86df54119d796be34f2c9dfa699fb297732bf4
examAtlas <crownclassesrnc@gmail.com>
2024-09-13 17:55:27 +0530

    setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const multer = require("multer")
+const dotenv = require("dotenv")
+
+dotenv.config({path:"./.env"})
+
+const app = express();
+app.use(express.json())
+app.use(multer().any());
+
+mongoose.connect(process.env.DB).then(()=>{
+  console.log("mongodb is connected.")
+}).catch((error)=>{
+  console.log("mongodb error:",error)
+})
+
+const port = process.env.PORT
+
+app.listen(port,function(){
+  console.log(`Server is running on port ${port}.`)
+})
