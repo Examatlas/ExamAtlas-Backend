099e73acb3d3b5f981098f19fbb4fc673c097c29
examAtlas <crownclassesrnc@gmail.com>
2024-10-02 17:58:28 +0530

    razorpay

--- index.js
+++ index.js
@@ -1,46 +1,109 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const multer = require("multer")
-const dotenv = require("dotenv")
-const userRoute = require("./src/Routes/user")
-const blogRoute = require("./src/Routes/Blog")
-const LiveClass = require("./src/Routes/LiveClassRoute")
-const CurrentAffair = require("./src/Routes/CurrentAffair")
+// const express = require("express")
+// const mongoose = require("mongoose")
+// const multer = require("multer")
+// const dotenv = require("dotenv")
+// const userRoute = require("./src/Routes/user")
+// const blogRoute = require("./src/Routes/Blog")
+// const LiveClass = require("./src/Routes/LiveClassRoute")
+// const CurrentAffair = require("./src/Routes/CurrentAffair")
+// const CourseRoute = require("./src/Routes/Courses");
+// const CategoryRoute = require("./src/Routes/Category")
+// const BookRoute = require("./src/Routes/Book");
+// const WishlistRoute = require("./src/Routes/Wishlist");
+// const cartRoute = require("./src/Routes/Cart")
+// const BillingRoute = require("./src/Routes/BillingDetail")
+
+// const PaymentRoute = require("./src/Routes/PaymentRoute");
+
+// const cors = require('cors');
+// dotenv.config({path:"./.env"})
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json())
+
+// //api
+// app.use("/api/user",userRoute);
+// app.use("/api/blog",blogRoute);
+// app.use("/api/liveclass",LiveClass);
+// app.use("/api/currentAffair",CurrentAffair)
+// app.use("/api/course",CourseRoute)
+// app.use("/api/category",CategoryRoute)
+// app.use("/api/book",BookRoute)
+// app.use("/api/wishlist",WishlistRoute)
+// app.use("/api/cart",cartRoute);
+// app.use("/api/billing",BillingRoute)
+
+// app.use("/api",PaymentRoute)
+// app.get("/api/getkey", (req, res) =>
+//   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
+// );
+
+// mongoose.connect(process.env.DB).then(()=>{
+//   console.log("mongodb is connected.")
+// }).catch((error)=>{
+//   console.log("mongodb error:",error)
+// })
+
+
+// const port = process.env.PORT
+
+// app.listen(port,function(){
+//   console.log(`Server is running on port ${port}.`)
+// })
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
+const userRoute = require("./src/Routes/user");
+const blogRoute = require("./src/Routes/Blog");
+const LiveClass = require("./src/Routes/LiveClassRoute");
+const CurrentAffair = require("./src/Routes/CurrentAffair");
 const CourseRoute = require("./src/Routes/Courses");
-const CategoryRoute = require("./src/Routes/Category")
+const CategoryRoute = require("./src/Routes/Category");
 const BookRoute = require("./src/Routes/Book");
 const WishlistRoute = require("./src/Routes/Wishlist");
-const cartRoute = require("./src/Routes/Cart")
-const BillingRoute = require("./src/Routes/BillingDetail")
-
-const cors = require('cors');
-dotenv.config({path:"./.env"})
+const cartRoute = require("./src/Routes/Cart");
+const BillingRoute = require("./src/Routes/BillingDetail");
+const PaymentRoute = require("./src/Routes/PaymentRoute");
+const Razorpay = require("razorpay");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-//api
-app.use("/api/user",userRoute);
-app.use("/api/blog",blogRoute);
-app.use("/api/liveclass",LiveClass);
-app.use("/api/currentAffair",CurrentAffair)
-app.use("/api/course",CourseRoute)
-app.use("/api/category",CategoryRoute)
-app.use("/api/book",BookRoute)
-app.use("/api/wishlist",WishlistRoute)
-app.use("/api/cart",cartRoute);
-app.use("/api/billing",BillingRoute)
+app.use("/api/user", userRoute);
+app.use("/api/blog", blogRoute);
+app.use("/api/liveclass", LiveClass);
+app.use("/api/currentAffair", CurrentAffair);
+app.use("/api/course", CourseRoute);
+app.use("/api/category", CategoryRoute);
+app.use("/api/book", BookRoute);
+app.use("/api/wishlist", WishlistRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/billing", BillingRoute);
+app.use("/api", PaymentRoute);
 
-mongoose.connect(process.env.DB).then(()=>{
-  console.log("mongodb is connected.")
-}).catch((error)=>{
-  console.log("mongodb error:",error)
-})
+app.get("/api/getkey", (req, res) => {
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
+});
 
-const port = process.env.PORT
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("mongodb is connected!.");
+  })
+  .catch((error) => {
+    console.log("mongodb error:", error);
+  });
 
-app.listen(port,function(){
-  console.log(`Server is running on port ${port}.`)
-})
+const port = process.env.PORT;
 
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}.`);
+});
