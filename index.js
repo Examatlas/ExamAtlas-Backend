af5add158cadbc75c1bde84140378b5bd326c09c
VikramRai-Mumbai <vikramraimumbai@gmail.com>
2024-10-18 18:48:13 +0530

    changes done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res
     .status(200)
     .send({ message: "Welcome to ExamAtlas backend portal.",
-  updated_at: "17-10-2024 05:30 PM IST" });
+  updated_at: "18-10-2024 06:47 PM IST" });
     });
 
 
